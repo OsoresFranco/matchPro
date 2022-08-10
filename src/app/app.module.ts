f1592a62c73ea65cdd802c4373d9b7ee7f0b2e6c
Franco_Osores <franco_osores@hotmail.com>
2022-08-10 20:09:51 -0300

    Feat: added material module for better organization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FeaturesModule } from './features/features.module';
+import { MaterialModule } from './material/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { FeaturesModule } from './features/features.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FeaturesModule
+    FeaturesModule,
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../material/material.module';
 
 import { FeaturesRoutingModule } from './features-routing.module';
 import { HomeComponent } from './views/home/home.component';
@@ -7,17 +8,8 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { BetComponent } from './views/bet/bet.component';
 
-
 @NgModule({
-  declarations: [
-    HomeComponent,
-    NavbarComponent,
-    FooterComponent,
-    BetComponent
-  ],
-  imports: [
-    CommonModule,
-    FeaturesRoutingModule
-  ]
+  declarations: [HomeComponent, NavbarComponent, FooterComponent, BetComponent],
+  imports: [CommonModule, FeaturesRoutingModule, MaterialModule],
 })
-export class FeaturesModule { }
+export class FeaturesModule {}
