d6e6e613e1803d64814d8af086b080fa58b1fb48
Franco_Osores <franco_osores@hotmail.com>
2022-08-18 12:59:31 -0300

    Feat: Navbar layout finished

--- src/app/features/features-routing.module.ts
+++ src/app/features/features-routing.module.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from './views/home/home.component';
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: '',
     component: HomeComponent,
     children: [
       {
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatButtonModule],
-  exports: [MatButtonModule],
+  imports: [CommonModule],
+  exports: [MatButtonModule, MatMenuModule],
 })
 export class MaterialModule {}
