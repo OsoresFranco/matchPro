3a7baaa71109b862f6d68314cad5c51a990b66b0
Franco_Osores <franco_osores@hotmail.com>
2022-09-03 04:03:33 -0300

    Fix, close of dialogs

--- src/app/features/features-routing.module.ts
+++ src/app/features/features-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '../core/guards/auth.guard';
 import { BetComponent } from './views/bet/bet.component';
 import { FeaturesComponent } from './views/features.component';
 import { HomeComponent } from './views/home/home.component';
@@ -14,6 +15,7 @@ const routes: Routes = [
   {
     path: '',
     component: FeaturesComponent,
+    canActivate: [AuthGuard],
     children: [
       { path: 'home', component: HomeComponent },
       { path: 'bet', component: BetComponent },
